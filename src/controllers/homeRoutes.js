c04b2a594eb4be1734db777937ca3aa13ddf7ad3
kalynsifuentez <kalynsifuentez@gmail.com>
2024-03-13 16:27:28 -0500

    added movie picker logic and model for the movie picker

--- src/controllers/homeRoutes.js
+++ src/controllers/homeRoutes.js
@@ -15,4 +15,42 @@ router.get("/", async (req, res) => {
     }
   });
 
+  // Login
+router.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: "Incorrect username or password, please try again" });
+      return;
+    }
+
+    const validPassword = userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: "Incorrect email or password, please try again" });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.userId = userData.id;
+      req.session.loggedIn = true;
+
+      res
+        .status(200)
+        .json({ user: userData, message: "You are now logged in!" });
+    });
+  } catch (err) {
+    res.status(400).json(err);
+    console.log(err);
+  }
+});
+
+
 module.exports = router;
